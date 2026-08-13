f930f6c54383da1724c9985c94567096d02c052e
MiniMatii <mitchelmejor@hotmail.com>
2026-08-13 14:39:48 -0300

    /

---
+++ OperariosDesktop/Forms/Inicio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KimTools.WinForms;
+
+namespace OperariosDesktop.Forms
+{
+    public partial class Inicio : KtWindow
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
