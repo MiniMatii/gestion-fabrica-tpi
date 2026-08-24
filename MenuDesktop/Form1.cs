c74a5291b94ade360255816b1a91f99548c806b8
matia <matia@MINIMATI>
2026-08-24 19:00:30 -0300

    Integracion de interfaz visual - inicio

---
+++ MenuDesktop/Form1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using KimTools;
+using KimTools.WinForms;
+
+namespace MenuDesktop
+
+{
+    public partial class Form1 : KtFormTools
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
